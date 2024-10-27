642318af55f04c4ab0059daee7d142d2ef683187
Rithik <rithik-dev>
2024-10-27 22:02:24 +0530

    fix: cors issue

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 const  corsOptions = {
-  origin: 'http//localhost:5173',
-  Credentials: true
+  origin: 'http://localhost:5173',
+  credentials: true
 }
 
 app.use(cors(corsOptions));
